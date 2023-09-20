1502e1593050989627919d11442ec2689b34c1c0
Kueffner, Maximilian <maximilian.kueffner@zeiss.com>
2023-09-20 14:31:59 +0200

    expose parser functions

--- src/package.lisp
+++ src/package.lisp
@@ -1,3 +1,15 @@
 (defpackage line-parser
   (:use :cl :clingon :cl-ppcre)
-  (:export :main))
+  (:export
+   :main
+   :warning-filter-tmp-file
+   :warning-filter
+   :drop-after
+   :duplicate-free-warnings
+   :check-all-warnings
+   :extract-from-strings
+   :filter-all-found-warnings/errors
+   :compare-filtered-statistics
+   :save-compared-results
+   :save-all-warnings-statistic-results
+   :save-all-warnings-with-filtered-output))
